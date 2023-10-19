a29d94db0b2e5b109fd0ff936bf2131964c8c28f
Angela-GM <angelagm0112@gmail.com>
2023-10-19 14:18:27 +0200

    fix typo in error-page

--- src/error-page.tsx
+++ src/error-page.tsx
@@ -1,16 +1,50 @@
 import { useRouteError } from "react-router-dom";
+interface RouterError {
+  statusText: string;
+  message: string;
+}
 
 export default function ErrorPage() {
-  const error: unknown= useRouteError();
+  const error = useRouteError();
   console.error(error);
 
-  return (
-    <div id="error-page">
-      <h1>Oops!</h1>
-      <p>Sorry, an unexpected error has occurred.</p>
-      <p>
-        <i>{error.statusText || error.message}</i>
-      </p>
-    </div>
-  );
+  if (error) {
+    if (
+      typeof error === "object" &&
+      "statusText" in error &&
+      "message" in error
+    ) {
+      const routerError = error as RouterError;
+
+      return (
+        <div id="error-page">
+          <h1>Oops!</h1>
+          <p>Sorry, an unexpected error has occurred.</p>
+          <p>
+            <i>{routerError.statusText || routerError.message}</i>
+          </p>
+        </div>
+      );
+    } else {
+      return (
+        <div id="error-page">
+          <h1>Oops!</h1>
+          <p>Sorry, an unexpected error has occurred.</p>
+          <p>
+            <i>Unknown error</i>
+          </p>
+        </div>
+      );
+    }
+  } else {
+    return (
+      <div id="error-page">
+        <h1>Oops!</h1>
+        <p>Sorry, an unexpected error has occurred.</p>
+        <p>
+          <i>No error data available</i>
+        </p>
+      </div>
+    );
+  }
 }
--- src/routes/root.tsx
+++ src/routes/root.tsx
@@ -1,50 +1,27 @@
 import { Link, Outlet } from "react-router-dom";
 
 export default function Root() {
-    return (
-      <>
-        <div id="sidebar">
-          <h1>Forest Fires Detected</h1>
-          {/* <div>
-            <form id="search-form" role="search">
-              <input
-                id="q"
-                aria-label="Search contacts"
-                placeholder="Search"
-                type="search"
-                name="q"
-              />
-              <div
-                id="search-spinner"
-                aria-hidden
-                hidden={true}
-              />
-              <div
-                className="sr-only"
-                aria-live="polite"
-              ></div>
-            </form>
-            <form method="post">
-              <button type="submit">New</button>
-            </form>
-          </div> */}
-          <nav className="py-4">
-            <ul className="flex justify-around">
-              <li>
+  return (
+    <>
+      <div id="sidebar">
+        <h1>Forest Fires Detected</h1>
+        <nav className="py-4">
+          <ul className="flex justify-around">
+            <li>
               <Link to={`table`}>Table</Link>
-              </li>
-              <li>
+            </li>
+            <li>
               <Link to={`map`}>Map</Link>
-              </li>
-              <li>
+            </li>
+            <li>
               <Link to={`search`}>Search</Link>
-              </li>
-            </ul>
-          </nav>
-        </div>
-        <div id="detail">
-            <Outlet />
-        </div>
-      </>
-    );
-  }
+            </li>
+          </ul>
+        </nav>
+      </div>
+      <div id="detail">
+        <Outlet />
+      </div>
+    </>
+  );
+}
