1765ef94922a97d4faf39f69808175fb18ba0a6d
Angela-GM <angelagm0112@gmail.com>
2023-10-20 14:07:40 +0200

    add map in MapComponent

--- src/components/MapComponent.tsx
+++ src/components/MapComponent.tsx
@@ -1,6 +1,9 @@
 function MapComponent() {
   return (
-    <div>Map Component</div>
+    <div className="my-4">
+    
+    <iframe className="w-screen h-screen" src="https://analisis.datosabiertos.jcyl.es/explore/embed/dataset/incendios-forestales/custom/?disjunctive.provincia&disjunctive.situacion_actual&refine.provincia=%C3%81VILA&static=false&datasetcard=false" frameborder="0"></iframe>
+    </div>
   )
 }
 
